3f7787e66fa3a5fcb6ed1ad179eba814538eaeff
Ls24567 <124741361+Jujhtghtfhh@users.noreply.github.com>
2025-04-06 02:01:14 +0300

    Update ComputerCanvas.jsx

--- src/components/ComputerCanvas.jsx
+++ src/components/ComputerCanvas.jsx
@@ -10,110 +10,130 @@ import game from "../game/Game.js";
 const clickSounds = [click1, click2, click3, click4];
 
 const playClickSound = () => {
-  const randomSfx = clickSounds[Math.floor(Math.random() * clickSounds.length)];
-  const sound = new Audio(randomSfx);
-  sound.playbackRate = 1 + Math.random() * 0.3;
-  sound.volume = 0.05 + Math.random() * 0.02;
-  sound.play();
+	const randomSfx = clickSounds[Math.floor(Math.random() * clickSounds.length)];
+	const sound = new Audio(randomSfx);
+	sound.playbackRate = 1 + Math.random() * 0.3;
+	sound.volume = 0.05 + Math.random() * 0.02;
+	sound.play();
 };
 
 const ComputerCanvas = ({ onClick }) => {
-  const [text, addText] = useState([]);
-  const [consoleStyles, setConsoleStyles] = useState();
+	const [text, addText] = useState([]);
+	const [consoleStyles, setConsoleStyles] = useState();
 
-  const canvasRef = useRef(null);
-  const appRef = useRef(null);
-  const spriteRef = useRef(null);
+	const canvasRef = useRef(null);
+	const appRef = useRef(null);
+	const spriteRef = useRef(null);
+	const foregroundRef = useRef(null);
 
-  function handleClick(sprite) {
-    game.resourceManager.addEuros(1);
-    playClickSound();
-    addText((prev) => [...prev, "test"]);
-    if (onClick) onClick();
-  }
+	function handleClick(sprite) {
+		game.resourceManager.addEuros(1);
+		playClickSound();
+		addText((prev) => [...prev, "test"]);
+		if (onClick) onClick();
+	}
 
-  function handleResize() {
-    if (!appRef.current) return;
-    if (!spriteRef.current) return;
-    const app = appRef.current;
-    const sprite = spriteRef.current;
-    sprite.setSize(app.screen.height / 2);
-    sprite.position.set(app.screen.width / 2, app.screen.height / 2);
-  }
+	function handleResize() {
+		if (!appRef.current || !spriteRef.current || !foregroundRef.current) return;
 
-  useEffect(() => {
-    if (!canvasRef.current) return;
-    let load = async () => {
-      const app = new PIXI.Application();
-      app.stage.sortableChildren = true;
-      appRef.current = app;
+		const app = appRef.current;
+		const sprite = spriteRef.current;
+		const foreground = foregroundRef.current;
 
-      await app.init({ resizeTo: canvasRef.current, backgroundAlpha: 0 });
-      canvasRef.current.appendChild(app.canvas);
+		const spriteSize = app.screen.height / 2;
+		sprite.setSize(spriteSize);
+		sprite.position.set(app.screen.width / 2, app.screen.height / 2);
 
-      const texture = await PIXI.Assets.load("src/assets/pc1.png");
+		foreground.width = sprite.width;
+		foreground.height = sprite.height;
+		foreground.position.set(
+			sprite.position.x,
+			sprite.position.y
+		);
+	}
 
-      const sprite = new PIXI.Sprite(texture);
-      spriteRef.current = sprite;
-      sprite.setSize(app.screen.height / 2);
-      sprite.anchor.set(0.5);
-      sprite.position.set(app.screen.width / 2, app.screen.height / 2);
-      sprite.eventMode = "static";
-      sprite.cursor = "pointer";
-      sprite.on("pointerdown", () => {
-        handleClick(sprite);
-      });
-      new ResizeObserver(handleResize).observe(canvasRef.current);
+	useEffect(() => {
+		if (!canvasRef.current) return;
+		let load = async () => {
+			const app = new PIXI.Application();
+			app.stage.sortableChildren = true;
+			appRef.current = app;
 
-      app.stage.addChild(sprite);
-      app.ticker.add((time) => {
-        const spriteBounds = sprite.getBounds(false, new PIXI.Bounds());
-        const spriteWidth = spriteBounds.maxX - spriteBounds.minX;
-        const spriteHeight = spriteBounds.maxY - spriteBounds.minY;
-        const originalSpriteWidth = 1024;
-        const originalSpriteHeight = 1024;
-        const targetTopLeft = { x: 208, y: 164 };
-        const targetBottomRight = { x: 815, y: 563 };
+			await app.init({ resizeTo: canvasRef.current, backgroundAlpha: 0 });
+			canvasRef.current.appendChild(app.canvas);
 
-        if (spriteBounds)
-          setConsoleStyles({
-            left: Math.floor(
-              spriteBounds.minX +
-                targetTopLeft.x * (spriteWidth / originalSpriteWidth)
-            ),
-            top: Math.floor(
-              spriteBounds.minY +
-                targetTopLeft.y * (spriteHeight / originalSpriteHeight)
-            ),
-            width: Math.ceil(
-              (targetBottomRight.x - targetTopLeft.x + 1) *
-                (spriteWidth / originalSpriteWidth)
-            ),
-            height:
-              Math.ceil(
-                (targetBottomRight.y - targetTopLeft.y + 1) *
-                  (spriteHeight / originalSpriteHeight)
-              ) + 1,
-          });
-      });
-    };
+			const [computerTexture, foregroundTexture] = await Promise.all([
+				PIXI.Assets.load("src/assets/pc1.png"),
+				PIXI.Assets.load("src/assets/foreground.png")
+			]);
 
-    load();
+			const foreground = new PIXI.Sprite(foregroundTexture);
+			foregroundRef.current = foreground;
+			foreground.anchor.set(0.5);
+			foreground.zIndex = -1;
+			app.stage.addChild(foreground);
 
-    return () => {
-      appRef.current.destroy(true);
-    };
-  }, []);
+			const sprite = new PIXI.Sprite(computerTexture);
+			spriteRef.current = sprite;
+			sprite.anchor.set(0.5);
+			sprite.eventMode = "static";
+			sprite.cursor = "pointer";
+			sprite.zIndex = 1;
+			sprite.on("pointerdown", () => {
+				handleClick(sprite);
+			});
 
-  return (
-    <div
-      onResize={handleResize}
-      ref={canvasRef}
-      style={{ position: "relative", width: "100%", height: "100%" }}
-    >
-      <Console textState={text} styles={consoleStyles} />
-    </div>
-  );
+			app.stage.addChild(sprite);
+			new ResizeObserver(handleResize).observe(canvasRef.current);
+
+			app.ticker.add((time) => {
+				const spriteBounds = sprite.getBounds(false, new PIXI.Bounds());
+				const spriteWidth = spriteBounds.maxX - spriteBounds.minX;
+				const spriteHeight = spriteBounds.maxY - spriteBounds.minY;
+				const originalSpriteWidth = 1024;
+				const originalSpriteHeight = 1024;
+				const targetTopLeft = { x: 208, y: 164 };
+				const targetBottomRight = { x: 815, y: 563 };
+
+				if (spriteBounds)
+					setConsoleStyles({
+						left: Math.floor(
+							spriteBounds.minX +
+							targetTopLeft.x * (spriteWidth / originalSpriteWidth)
+						),
+						top: Math.floor(
+							spriteBounds.minY +
+							targetTopLeft.y * (spriteHeight / originalSpriteHeight)
+						),
+						width: Math.ceil(
+							(targetBottomRight.x - targetTopLeft.x + 1) *
+							(spriteWidth / originalSpriteWidth)
+						),
+						height:
+							Math.ceil(
+								(targetBottomRight.y - targetTopLeft.y + 1) *
+								(spriteHeight / originalSpriteHeight)
+							) + 1,
+					});
+			});
+		};
+
+		load();
+
+		return () => {
+			appRef.current?.destroy(true);
+		};
+	}, []);
+
+	return (
+		<div
+			onResize={handleResize}
+			ref={canvasRef}
+			style={{ position: "relative", width: "100%", height: "100%" }}
+		>
+			<Console textState={text} styles={consoleStyles} />
+		</div>
+	);
 };
 
 export default ComputerCanvas;
