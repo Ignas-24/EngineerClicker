047d9120a668c550c2cf1ba6556b156c58f0549d
ignas24 <ignaspyrantas@gmail.com>
2025-05-22 23:34:01 +0300

    Updated vitest config to exclude playwright tests

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    include: ['src/**/*.test.js'],
+    exclude: ['tests/**/*.spec.ts'],
     coverage: {
       reporter: ["text", "json", "html"],
     },
