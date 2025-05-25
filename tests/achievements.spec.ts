079b56dab04a3f3b809fa1c8b1e6b255836f37c6
Ls24567 <124741361+Jujhtghtfhh@users.noreply.github.com>
2025-05-25 23:55:41 +0300

    Update achievements.spec.ts

--- tests/achievements.spec.ts
+++ tests/achievements.spec.ts
@@ -1,46 +1,53 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("clicking the computer 10000 times triggers an achievement", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:5173/EngineerClicker/");
 
-  await page.evaluate(() => {
-    const element = document.querySelector("div.console_wrap");
-    if (!element) throw new Error("Element not found");
+test('clicking the computer 10000 times triggers an achievement', async ({ page }) => {
 
-    const event = new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true,
-      view: window,
+    await page.goto('http://localhost:5173/EngineerClicker/');
+
+
+    await page.evaluate(() => {
+    const element = document.querySelector('div.console_wrap');
+    if (!element) throw new Error('Element not found');
+    
+    const event = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
     });
 
     for (let i = 0; i < 10000; i++) {
-      element.dispatchEvent(event);
-    }
-  });
+        element.dispatchEvent(event);
 
-  const alert = page.locator("label", {
-    hasText: "Achievement Unlocked: Almost like a real job",
-  });
-  await expect(alert).toHaveText(
-    "Achievement Unlocked: Almost like a real job",
-  );
+    }
+    });
+
+
+
+    const alert = page.locator('label', {
+        hasText: 'Achievement Unlocked: Almost like a real job',
+    });
+    await expect(alert).toHaveText('Achievement Unlocked: Almost like a real job');
+
 });
 
-test("user takes a maximum loan", async ({ page }) => {
-  await page.goto("http://localhost:5173/EngineerClicker/");
 
-  await page.getByRole("button", { name: "Bank" }).click();
+test('user takes a maximum loan', async ({ page }) => {
+    await page.goto('http://localhost:5173/EngineerClicker/'); 
 
-  const input = await page.locator("input").first();
-  await input.fill("5000");
+    await page.getByRole('button', { name: 'Bank' }).click();
 
-  await page.getByRole("button", { name: "Take loan" }).click();
+    const input = await page.locator('input').first();
+    await input.fill('5000');
 
-  const alert = page.locator("label", {
-    hasText: "Achievement Unlocked: Calculated risk",
-  });
+    await page.getByRole('button', { name: 'Take loan' }).click();
 
-  await expect(alert).toHaveText("Achievement Unlocked: Calculated risk");
+    const alert = page.locator('label', {
+        hasText: 'Achievement Unlocked: Calculated risk',
+    });
+
+    await expect(alert).toHaveText('Achievement Unlocked: Calculated risk');  
+
+  
+
 });
